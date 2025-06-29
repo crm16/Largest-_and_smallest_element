8df6841144a7fe1b0708986e21761e76a35ffc11
Chandrarit <130826166+crm16@users.noreply.github.com>
2025-06-29 20:13:18 +0530

    Largest_smallest_element.java

--- Largest_smallest_element.java
+++ Largest_smallest_element.java
@@ -2,30 +2,30 @@ import java.util.*;
 public class Largest_smallest_element {
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of elements: ");
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter number of elements to be compared: ");
         int size = sc.nextInt();
         System.out.println("Enter " + size + " elements: ");
 
-        int numbers[] = new int[size];
-        for(int i =0; i<size; i++){
-            numbers[i]=sc.nextInt();
+        int elements [] = new int[size];
+        for(int i = 0; i < size ; i++){
+            elements[i] = sc.nextInt();
         }
-        
+
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        
-        for(int i = 0; i<size; i++){
-            if(numbers[i]>max){
-                max = numbers[i];
+
+        for(int i = 0; i < elements.length; i++){
+            if(elements[i] > max){
+                max = elements[i];
             }
-            if(numbers[i]<min){
-                min = numbers[i];
+            if(elements[i] < min){
+                min = elements[i];
             }
         }
         
         System.out.print("\n");
-        System.out.println("Largest element: " + max);
-        System.out.println("Smallest element: " +min);
+        System.out.println("Largest element is: " + max);
+        System.out.println("Smallest element is: " + min);
     }
 }
